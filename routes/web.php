642ec9bf5b70975d2d8da89eb2e819a5b45ca268
Thiha-19 <thihaph1999@gmail.com>
2021-07-29 22:21:47 +0630

    ok

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
 Route::resource('/roles', 'App\Http\Controllers\RoleController');
